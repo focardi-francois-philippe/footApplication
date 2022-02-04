e299ee4ec18910e6f2d84a2f71e97af8598fe3e5
Focardi François-Philippe <francois.focardi@hotmail.fr>
2022-02-04 18:35:33 +0100

    first commit

---
+++ app/src/main/java/csc/l3/p2022/focardi/idreacealy/footapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package csc.l3.p2022.focardi.idreacealy.footapplication;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import csc.l3.p2022.focardi.idreacealy.footapplication.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
